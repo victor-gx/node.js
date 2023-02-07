32f22ad2a534f29b4efa804173864a367deb9b87
gx <2416457672@qq.com>
2023-02-07 23:29:55 +0800

    项目4

--- 项目/code/api_server/schema/user.js
+++ 项目/code/api_server/schema/user.js
@@ -8,10 +8,49 @@ const password = joi
   .pattern(/^[\S]{6,12}$/)
   .required()
 
+// 定义 id, nickname, email 的验证规则
+const id = joi.number().integer().min(1).required()
+const nickname = joi.string().required()
+const user_email = joi.string().email().required()
+
+// 定义验证 avatar 头像的验证规则
+const avatar = joi.string().dataUri().required()
+
 // 定义验证注册和登录表单数据的规则对象
 exports.reg_login_schema = {
   body: {
     username,
     password,
   },
+}
+
+// 验证规则对象 - 更新用户基本信息
+exports.update_userinfo_schema = {
+  // 需要对 req.body 里面的数据进行验证
+  body: {
+    id,
+    nickname,
+    email: user_email,
+  },
+}
+
+// 验证规则对象 - 重置密码
+exports.update_password_schema = {
+  body: {
+    // 使用 password 这个规则，验证 req.body.oldPwd 的值
+    oldPwd: password,
+    // 使用 joi.not(joi.ref('oldPwd')).concat(password) 规则，验证 req.body.newPwd 的值
+    // 解读：
+    // 1. joi.ref('oldPwd') 表示 newPwd 的值必须和 oldPwd 的值保持一致
+    // 2. joi.not(joi.ref('oldPwd')) 表示 newPwd 的值不能等于 oldPwd 的值
+    // 3. .concat() 用于合并 joi.not(joi.ref('oldPwd')) 和 password 这两条验证规则
+    newPwd: joi.not(joi.ref('oldPwd')).concat(password),
+  },
+}
+
+// 验证规则对象 - 更新头像
+exports.update_avatar_schema = {
+  body: {
+    avatar
+  }
 }
