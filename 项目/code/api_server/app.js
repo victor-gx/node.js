3564a21fbdd523ce1b13570fb1f934b9c490d0a6
gx <2416457672@qq.com>
2023-02-08 23:42:03 +0800

    项目5

--- 项目/code/api_server/app.js
+++ 项目/code/api_server/app.js
@@ -37,6 +37,10 @@ app.use('/api', userRouter)
 const userinfoRouter = require('./router/userinfo')
 // 注意：以 /my 开头的接口，都是有权限的接口，需要进行 Token 身份认证
 app.use('/my', userinfoRouter)
+// 导入并使用文章分类路由模块
+const artCateRouter = require('./router/artcate')
+// 为文章分类的路由挂载统一的访问前缀 /my/article
+app.use('/my/article', artCateRouter)
 
 // 定义错误级别的中间件
 app.use((err, req, res, next) => {
